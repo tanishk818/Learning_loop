bdda758427063544c998d77cf63407bd35667f66
Rohit Bhati <rohitbhati8833@gmail.com>
2022-05-02 21:14:43 +0530

    feedback form ui completed

--- models/feedback.js
+++ models/feedback.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const feedbackSchema = new Schema({
     username: String,
+    name: String,
     message: String,
     createdOn: new Date()
-
 });
 module.exports = mongoose.model('Comment', feedbackSchema);
