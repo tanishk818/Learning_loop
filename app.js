9640c0d01252cc4f2f057f6d72a258de6052078e
Rohit Bhati <rohitbhati8833@gmail.com>
2022-05-08 15:27:38 +0530

    minor changes in app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use('/', home);
 app.use('/lev', lev);
 
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving from Port ${port}`);
+});
